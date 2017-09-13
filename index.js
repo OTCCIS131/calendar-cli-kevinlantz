953bcbb7262ac3aac28534e785a82596d3eb8898
Unknown <kl0231582@otc.edu>
2017-09-13 01:11:16 -0500

    updating code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var chunk = require('lodash');
+var _ = require('lodash');
 
 const Moment = require('moment');
 const MomentRange = require('moment-range');
@@ -9,7 +9,18 @@ const chalk = require('chalk');
 
 let year = moment().range("year");
 
-// _.forEach(Array.from(year.by("months")), month => {
-//             console.log(_.pad(month.format("mmmm"));
 
-//         }
+
+_.forEach(Array.from(year.by('months')), month => {
+    console.log(_.pad(month.format('MMMM'), 26, ' '))
+    console.log('S   M   T   W   Th  F   S   ')
+
+    let monthRange = month.range('month')
+    let firstDay = monthRange.start.day()
+        //let lastDay = monthRange.end.day()
+
+
+    // console.log(firstDay + '     ' + lastDay)
+    console.log(firstDay);
+
+})
